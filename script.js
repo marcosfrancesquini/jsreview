aa7b3a6065eb82d65114e5946b63a397cceb40e0
marcosfrancesquini <francesquini.personal@gmail.com>
2025-02-26 16:03:58 -0300

    password css

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-let erros = [];
-
+// JavaScript para manipulação das mensagens de erro
 const mensagemErro = document.getElementById('mensagemErro');
 const btnRegistrar = document.getElementById('btnRegistrar');
+
+function mostrarMensagemErro(mensagens) {
+    mensagemErro.style.display = 'block';
+    mensagemErro.style.opacity = '1';
+    mensagemErro.innerHTML = ''; // Limpa o conteúdo anterior
+
+    mensagens.forEach(mensagem => {
+        const mensagemErroElement = document.createElement('h6');
+        mensagemErroElement.textContent = mensagem;
+        mensagemErro.appendChild(mensagemErroElement);
+    });
+}
 
 btnRegistrar.addEventListener('click', function (event) {
     event.preventDefault();
     const senha = document.getElementById('password').value;
-    const confirmaSenha = document.getElementById('confirmaSenha').value;
+    const confirmaSenha = document.getElementById('confirmPassword').value;
 
-    erros.length = 0; // Limpa o array de erros
+    let erros = [];
 
     if (senha !== confirmaSenha) {
         erros.push('As senhas não coincidem.');
@@ -17,34 +28,21 @@ btnRegistrar.addEventListener('click', function (event) {
         erros.push('A senha deve ter entre 6 e 8 caracteres.');
     }
     if (!/[A-Z]/.test(senha)) {
-        erros.push('A senha deve conter pelo menos uma letra maiúscula');
+        erros.push('A senha deve conter pelo menos uma letra maiúscula.');
     }
-
     if (!/[a-z]/.test(senha)) {
-        erros.push('A senha deve conter pelo menos uma letra minúscula');
+        erros.push('A senha deve conter pelo menos uma letra minúscula.');
     }
-
-    if (!/(?=.*[!@#$%^&*(),.?":{}|<>]{2,})/.test(senha)){
+    if (!/(?=.*[!@#$%^&*(),.?":{}|<>]{2,})/.test(senha)) {
         erros.push('A senha deve ter ao menos 2 caracteres especiais.');
-    };
-
-    if (!/[0-9]/.test(senha)){
+    }
+    if (!/[0-9]/.test(senha)) {
         erros.push('A senha deve conter pelo menos um número.');
     }
 
     if (erros.length === 0) {
-        mensagemErro.innerHTML = '<h6>Acesso concedido...</h6>';
+        mostrarMensagemErro(['Acesso concedido...']);
     } else {
-        mensagemErro.innerHTML = ''; // Limpa o conteúdo anterior
-        erros.forEach(erro => {
-            const mensagemErroElement = document.createElement('h6');
-            mensagemErroElement.textContent = erro;
-            mensagemErro.appendChild(mensagemErroElement);
-        });
+        mostrarMensagemErro(erros);
     }
 });
-
-
-
-
-
